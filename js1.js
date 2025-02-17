7aab82f7eb8f8ebe5488d945f61dc34626892253
MohamedAliHG <medalihajamgabsi@gmail.com>
2025-02-17 06:18:17 +0100

    update js1

--- js1.js
+++ js1.js
@@ -40,7 +40,7 @@ function getGameLevel(){
     do{
         game_level=prompt("enter game level: e (easy) i (intermediate) h (hard)");
 
-    }while(game_level.toLowerCase()!='e' && game_level.toLowerCase()!='i' && game_level.toLowerCase()!='h');
+    }while(!game_level && game_level.toLowerCase()!='e' && game_level.toLowerCase()!='i' && game_level.toLowerCase()!='h');
 
     return game_level.toLowerCase();
 }
